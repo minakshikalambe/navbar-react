b1f98e38a4e837882ae983726df83014a025378b
minakshikalambe <97457265+minakshikalambe@users.noreply.github.com>
2022-04-11 23:25:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='logobakery'>LOGOBAKERY</div>
+    <div className='services'>Services</div>
+    <div className='project'>Project</div>
+    <div className='about'>About</div>
+    <div className='contact'>
+      <button>Contact</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
